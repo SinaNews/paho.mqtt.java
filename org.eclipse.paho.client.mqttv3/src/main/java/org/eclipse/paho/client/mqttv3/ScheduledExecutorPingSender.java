f1ba7c11a246e0279b6a369c6f2e439f905bbba7
sunxiao <sunxiao5@staff.sina.com.cn>
2018-06-20 21:21:04 +0800

    更改配置

--- org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/ScheduledExecutorPingSender.java
+++ org.eclipse.paho.client.mqttv3/src/main/java/org/eclipse/paho/client/mqttv3/ScheduledExecutorPingSender.java
@@ -25,68 +25,64 @@ import org.eclipse.paho.client.mqttv3.logging.LoggerFactory;
 /**
  * Default ping sender implementation
  *
- * <p>This class implements the {@link IMqttPingSender} pinger interface
- * allowing applications to send ping packet to server every keep alive interval.
- * </p>
- *
  * @see MqttPingSender
  */
 public class ScheduledExecutorPingSender implements MqttPingSender {
-	private static final String CLASS_NAME = ScheduledExecutorPingSender.class.getName();
-	private static final Logger log = LoggerFactory.getLogger(LoggerFactory.MQTT_CLIENT_MSG_CAT, CLASS_NAME);
+    private static final String CLASS_NAME = ScheduledExecutorPingSender.class.getName();
+    private static final Logger log = LoggerFactory.getLogger(LoggerFactory.MQTT_CLIENT_MSG_CAT, CLASS_NAME);
 
-	private ClientComms comms;
-	private ScheduledExecutorService executorService;
-	private ScheduledFuture scheduledFuture;
-	private String clientid;
+    private ClientComms comms;
+    private ScheduledExecutorService executorService;
+    private ScheduledFuture scheduledFuture;
+    private String clientid;
 
-	public ScheduledExecutorPingSender(ScheduledExecutorService executorService) {
-		if (executorService == null) {
-			throw new IllegalArgumentException("ExecutorService cannot be null.");
-		}
-		this.executorService = executorService;
-	}
+    public ScheduledExecutorPingSender(ScheduledExecutorService executorService) {
+        if (executorService == null) {
+            throw new IllegalArgumentException("ExecutorService cannot be null.");
+        }
+        this.executorService = executorService;
+    }
 
-	public void init(ClientComms comms) {
-		if (comms == null) {
-			throw new IllegalArgumentException("ClientComms cannot be null.");
-		}
-		this.comms = comms;
-		clientid = comms.getClient().getClientId();
-	}
+    public void init(ClientComms comms) {
+        if (comms == null) {
+            throw new IllegalArgumentException("ClientComms cannot be null.");
+        }
+        this.comms = comms;
+        clientid = comms.getClient().getClientId();
+    }
 
-	public void start() {
-		final String methodName = "start";
+    public void start() {
+        final String methodName = "start";
 
-		//@Trace 659=start timer for client:{0}
-		log.fine(CLASS_NAME, methodName, "659", new Object[]{ clientid });
-		//Check ping after first keep alive interval.
-		schedule(comms.getKeepAlive());
-	}
+        //@Trace 659=start timer for client:{0}
+        log.fine(CLASS_NAME, methodName, "659", new Object[]{clientid});
+        //Check ping after first keep alive interval.
+        schedule(comms.getKeepAlive());
+    }
 
-	public void stop() {
-		final String methodName = "stop";
-		//@Trace 661=stop
-		log.fine(CLASS_NAME, methodName, "661", null);
-		if (scheduledFuture != null) {
-			scheduledFuture.cancel(true);
-		}
-	}
+    public void stop() {
+        final String methodName = "stop";
+        //@Trace 661=stop
+        log.fine(CLASS_NAME, methodName, "661", null);
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(true);
+        }
+    }
 
-	public void schedule(long delayInMilliseconds) {
-		scheduledFuture = executorService.schedule(new PingRunnable(), delayInMilliseconds, TimeUnit.MILLISECONDS);
-	}
+    public void schedule(long delayInMilliseconds) {
+        scheduledFuture = executorService.schedule(new PingRunnable(), delayInMilliseconds, TimeUnit.MILLISECONDS);
+    }
 
-	private class PingRunnable implements Runnable {
-		private static final String methodName = "PingTask.run";
+    private class PingRunnable implements Runnable {
+        private static final String methodName = "PingTask.run";
 
-		public void run() {
-			String originalThreadName = Thread.currentThread().getName();
-			Thread.currentThread().setName("MQTT Ping: " + clientid);
-			//@Trace 660=Check schedule at {0}
-			log.fine(CLASS_NAME, methodName, "660", new Object[]{ new Long(System.currentTimeMillis()) });
-			comms.checkForActivity();
-			Thread.currentThread().setName(originalThreadName);
-		}
-	}
+        public void run() {
+            String originalThreadName = Thread.currentThread().getName();
+            Thread.currentThread().setName("MQTT Ping: " + clientid);
+            //@Trace 660=Check schedule at {0}
+            log.fine(CLASS_NAME, methodName, "660", new Object[]{new Long(System.currentTimeMillis())});
+            comms.checkForActivity();
+            Thread.currentThread().setName(originalThreadName);
+        }
+    }
 }
